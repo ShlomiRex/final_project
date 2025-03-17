4eaf3482f08b2e315013d89543c5431ad6c7c46e
Shlomi <vgtvgy1@gmail.com>
2025-03-17 17:46:29 +0200

    finish attention

--- vae/vae_blocks.py
+++ vae/vae_blocks.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from attention import SelfAttention
+import einops
 
 class ResBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -42,3 +44,59 @@ class AttentionBlock(nn.Module):
         super.__init__()
         
         self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (b c h w)
+
+        residue = x 
+
+        b, c, h, w = x.shape()
+
+        # TODO: Check this is correct
+        x2 = einops.rearrange(x, 'b c h w -> b c (h w)')
+
+        # b c h w -> b c h*w
+        x = x.view(b, c, h*w) # ok
+
+        assert torch.equal(x, x2)
+
+
+
+        # TODO: Check this is correct
+        x2 = einops.rearrange(x, 'b c (h w) -> b (h w) c')
+
+        # b c h*w -> b h*w c
+        x = x.transpose(-1, -2) # ok
+
+        assert torch.equal(x, x2)
+
+
+
+
+        x = self.attention(x)
+
+
+
+        # TODO: Check this is correct
+        x2 = einops.rearrange(x, 'b (h w) c -> b c (h w)')
+
+        # b h*w c -> b c h*w
+        x = x.transpose(-1, -2)
+
+        assert torch.equal(x, x2)
+
+
+        # TODO: Check this is correct
+        x2 = einops.rearrange(x, 'b c (h w) -> b c h w')
+
+        # b c h*w -> b c h w
+        x = x.view((b, c, h, w))
+
+        assert torch.equal(x, x2)
+
+
+
+        x += residue
+
+        return x
